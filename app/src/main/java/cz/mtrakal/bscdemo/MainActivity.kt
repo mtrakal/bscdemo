d5d80f210d16f270a2e29101a29a006c58133fb4
Matěj Trakal <mtrakal@gmail.com>
2020-02-25 21:08:45 +0100

    Init commit, after start AS with empty project

---
+++ app/src/main/java/cz/mtrakal/bscdemo/MainActivity.kt
@@ -0,0 +1,18 @@
+package cz.mtrakal.bscdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cz.mtrakal.bscdemo.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/cz/mtrakal/bscdemo/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package cz.mtrakal.bscdemo.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
